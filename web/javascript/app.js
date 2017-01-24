450844556f766e87396539872ea7f5e1e2e7b862
WorrenGilgamesh <victor.oz@live.com>
2017-01-24 10:46:21 -0600

    Carpetas & session

---
+++ web/javascript/app.js
@@ -0,0 +1,54 @@
+(function(){
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyBnDguniX29bnW89KSLG17gAq5am4i6E9s",
+    authDomain: "itamlabweb.firebaseapp.com",
+    databaseURL: "https://itamlabweb.firebaseio.com",
+    storageBucket: "itamlabweb.appspot.com",
+    messagingSenderId: "345250983225"
+  };
+  firebase.initializeApp(config);
+
+  const txtEmail = document.getElementById('txtEmail');
+  const txtPassword = document.getElementById('txtPassword');
+  const btnLogin = document.getElementById('btnLogin');
+  const btnSignUp = document.getElementById('btnSignUp');
+  const btnLogout = document.getElementById('btnLogout');
+
+  btnLogin.addEventListener('click', e=> {
+  	//Get email and password
+  	const email = txtEmail.value;
+  	const pass = txtPassword.value;
+  	const auth = firebase.auth();
+  	//Sign in
+  	const promise =auth.signInWithEmailAndPassword(email, pass);
+  	promise.catch(e => console.log(e.message));
+  })
+
+}());
+
+//Signup event
+btnSignUp.addEventListener('click', e => {
+	//Get email and password
+	//TODO: CHECK 4 REAL EMAILZ
+  	const email = txtEmail.value;
+  	const pass = txtPassword.value;
+  	const auth = firebase.auth();
+  	//Sign in
+  	const promise =auth.createUserWithEmailAndPassword(email, pass);
+  	promise.catch(e => console.log(e.message));
+})
+
+	btnLogout.addEventListener('click', e=>{
+		firebase.auth().signOut();
+	})
+
+//realtime Listener
+firebase.auth().onAuthStateChanged(firebaseUser => {
+	if (firebaseUser) {
+		console.log(firebaseUser);
+		btnLogout.classList.remove('hide');
+	}else
+	console.log('not log in')
+	btnLogout.classList.add('hide');
+});
