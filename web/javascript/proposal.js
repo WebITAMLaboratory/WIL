6877be65e9e1a905745374e5ff598b914d1a2ee4
WorrenGilgamesh <victor.oz@live.com>
2017-01-30 20:56:52 -0600

    WIL 1.0.5

--- web/javascript/proposal.js
+++ web/javascript/proposal.js
@@ -1,4 +1,43 @@
 (function() {
+   
+btnSignUp.addEventListener('click', e => {
+  const txtName = document.getElementById('txtName');
+  const txtMail = document.getElementById('txtMail');
+  const txtProcedencia = document.getElementById('txtProcedencia')
+   //TODO: CHECK 4 REAL EMAILZ
+     const name = txtName.value;
+     const mail = txtMail.value;
+     const procedencia = txtProcedencia.value;     
+     if (name != "" && mail != "" && procedencia="") {
+       if (/@itam.mx\s*$/.test(email)) {
+          $("#p2").show();
+          $("#btnSignUp").hide();     
+
+       function randomString(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+    return result;}
+
+
+     var user = randomString(10, '0123456789');
+     var  firebaseRef = firebase.database().ref(user);
+     firebaseRef.child("Name").set(name);
+     firebaseRef.child("mial").set(mail);
+     firebaseRef.child("EmailConfirm").set(procedencia);
+     alert("Successful Push");
+
+     }else{
+      if (/@itam.mx\s*$/.test(email) == false) {
+        alert("Only @itam.mx domains");
+      }else{
+       alert("Mising Fields");
+      }
+     }
+    }else{
+       alert("Mising Fields or email/password error");
+     }
+ });
+
    var storage = firebase.storage();
   // GET ELEMENT
   const uploader = document.getElementById('uploader');
